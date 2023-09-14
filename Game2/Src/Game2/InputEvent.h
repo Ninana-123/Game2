a6c24ea50f03086ab43088b07a5359c2946e1eb6
lxujie <liu_xujie@hotmail.sg>
2023-09-15 03:05:16 +0800

    Event Edits

--- Game2/Src/Game2/InputEvent.h
+++ Game2/Src/Game2/InputEvent.h
@@ -13,13 +13,13 @@ namespace Engine {
 
 		EVENT_CLASS_CATEGORY(EventCategoryKeyboard|EventCategoryInput)
 	protected:
-		KeyEvent(int keycode) : m_KeyCode(keycode) {}
+		KeyEvent(Logger& logger,int keycode) : Event(logger), m_KeyCode(keycode) {}
 		int m_KeyCode;
 	};
 
 	class GAME2_API KeyPressedEvent : public KeyEvent {
 	public:
-		KeyPressedEvent(int keycode, int repeatCount) : KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+		KeyPressedEvent(Logger& logger, int keycode, int repeatCount) : KeyEvent(logger, keycode), m_RepeatCount(repeatCount) {}
 
 		inline int GetRepeatCount() const { return m_RepeatCount; }
 		std::string ToString() const override {
@@ -35,7 +35,7 @@ namespace Engine {
 
 	class GAME2_API KeyReleasedEvent : public KeyEvent {
 	public:
-		KeyReleasedEvent(int keycode): KeyEvent(keycode) {}
+		KeyReleasedEvent(Logger& logger, int keycode) : KeyEvent(logger, keycode) {}
 
 		std::string ToString() const override {
 			std::stringstream ss;
@@ -47,74 +47,82 @@ namespace Engine {
 	};
 
 	//Mouse Inputs
-	class GAME2_API MouseMovedEvent : public Event {
-	public:
-		MouseMovedEvent(float x, float y) : m_MouseX(x), m_MouseY(y) {}
-		inline float GetX() const { return m_MouseX; }
-		inline float GetY() const { return m_MouseY; }
+    class GAME2_API MouseMovedEvent : public Event {
+    public:
+        MouseMovedEvent(Logger& logger, float x, float y) : Event(logger), m_MouseX(x), m_MouseY(y) {}
 
-		std::string ToString() const override {
-			std::stringstream ss;
-			ss << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
-			return ss.str();
-		}
+        inline float GetX() const { return m_MouseX; }
+        inline float GetY() const { return m_MouseY; }
 
-		EVENT_CLASS_TYPE(MouseMoved)
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
-	private:
-		float m_MouseX, m_MouseY;
-	};
+        std::string ToString() const override {
+            std::stringstream ss;
+            ss << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
+            return ss.str();
+        }
 
-	class GAME2_API MouseScrolledEvent : public Event {
-	public:
-		MouseScrolledEvent(float xOffset, float yOffset) : m_XOffset(xOffset), m_YOffset(yOffset) {}
-		inline float GetXOffset() const { return m_XOffset; }
-		inline float GetYOffset() const { return m_YOffset; }
+        EVENT_CLASS_TYPE(MouseMoved)
+            EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 
-		std::string ToString() const override {
-			std::stringstream ss;
-			ss << "MouseScrolledEvent: " << GetXOffset << ", " << GetYOffset;
-			return ss.str();
-		}
+    private:
+        float m_MouseX, m_MouseY;
+    };
 
-		EVENT_CLASS_TYPE(MouseScrolled)
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
-	private:
-		float m_XOffset, m_YOffset;
-	};
+    class GAME2_API MouseScrolledEvent : public Event {
+    public:
+        MouseScrolledEvent(Logger& logger, float xOffset, float yOffset) : Event(logger), m_XOffset(xOffset), m_YOffset(yOffset) {}
 
-	class GAME2_API MouseButtonEvent : public Event {
-	public:
-		inline int GetMouseButton() const { return m_Button; }
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
-	protected:
-		MouseButtonEvent(int button) : m_Button(button){}
-		int m_Button;
-	};
+        inline float GetXOffset() const { return m_XOffset; }
+        inline float GetYOffset() const { return m_YOffset; }
 
-	class GAME2_API MouseButtonPressedEvent : public MouseButtonEvent {
-	public:
-		MouseButtonPressedEvent(int button) : MouseButtonEvent(button) {}
+        std::string ToString() const override {
+            std::stringstream ss;
+            ss << "MouseScrolledEvent: " << GetXOffset() << ", " << GetYOffset();
+            return ss.str();
+        }
 
-		std::string ToString() const override {
-			std::stringstream ss;
-			ss << "MouseButtonPressedEvent: " << m_Button;
-			return ss.str();
-		}
+        EVENT_CLASS_TYPE(MouseScrolled)
+            EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 
-		EVENT_CLASS_TYPE(MouseButtonPressed)
-	};
+    private:
+        float m_XOffset, m_YOffset;
+    };
 
-	class GAME2_API MouseButtonReleasedEvent : public MouseButtonEvent {
-	public:
-		MouseButtonReleasedEvent(int button) : MouseButtonEvent(button) {}
+    class GAME2_API MouseButtonEvent : public Event {
+    public:
+        inline int GetMouseButton() const { return m_Button; }
 
-		std::string ToString() const override {
-			std::stringstream ss;
-			ss << "MouseButtonReleasedEvent: " << m_Button;
-			return ss.str();
-		}
+        EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 
-		EVENT_CLASS_TYPE(MouseButtonReleased)
-	};
+    protected:
+        MouseButtonEvent(Logger& logger, int button) : Event(logger), m_Button(button) {}
+
+    private:
+        int m_Button;
+    };
+
+    class GAME2_API MouseButtonPressedEvent : public MouseButtonEvent {
+    public:
+        MouseButtonPressedEvent(Logger& logger, int button) : MouseButtonEvent(logger, button) {}
+
+        std::string ToString() const override {
+            std::stringstream ss;
+            ss << "MouseButtonPressedEvent: " << GetMouseButton();
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseButtonPressed)
+    };
+
+    class GAME2_API MouseButtonReleasedEvent : public MouseButtonEvent {
+    public:
+        MouseButtonReleasedEvent(Logger& logger, int button) : MouseButtonEvent(logger, button) {}
+
+        std::string ToString() const override {
+            std::stringstream ss;
+            ss << "MouseButtonReleasedEvent: " << GetMouseButton();
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseButtonReleased)
+    };
 }
