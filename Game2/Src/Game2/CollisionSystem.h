b291752bdfad31433d299e0d08c531766aa1f55d
Aloy <aloyang123@gmail.com>
2023-09-29 04:43:17 +0800

    Added comments for collision

--- Game2/Src/Game2/CollisionSystem.h
+++ Game2/Src/Game2/CollisionSystem.h
@@ -53,12 +53,69 @@ namespace Engine
 			float radius;
 		};
 
+		/*!*****************************************************************
+
+		 \brief
+			The logic of AABB collision.
+
+		\param[in] aabb1
+			The bound of the first object.
+
+		\param[in] vel1
+			The velocity of the first object.
+
+		\param[in] aabb2
+			The bound of the second object.
+
+		\param[in] vel2
+			The velocity of the second object.
+
+		\return  boolean
+			Returns true if there is a collision, false is there is not.
+
+		********************************************************************/
 		bool CollisionIntersection_RectRect(const AABB& aabb1, const VECTORMATH::Vec2& vel1,
 			const AABB& aabb2, const VECTORMATH::Vec2& vel2);
 
+		/**************************************************************************/
+		/*!
+		\brief Determines if there is an intersection between two moving circles.
+
+		\param[in] circle1
+			The first Circle object representing the first moving circle.
+
+		\param[in] vel1
+			The velocity vector of the first circle.
+
+		\param[in] circle2
+			The second Circle object representing the second moving circle.
+
+		\param[in] vel2
+			The velocity vector of the second circle.
+
+		\return
+			True if the two moving circles intersect during their motion, false otherwise.
+
+		*/
+		/**************************************************************************/
 		bool CollisionIntersection_CircleCircle(const Circle& circle1, const VECTORMATH::Vec2& vel1,
 			const Circle& circle2, const VECTORMATH::Vec2& vel2);
 
+		/**************************************************************************/
+		/*!
+		\brief Determines if there is an intersection between a circle and an axis-aligned bounding box (AABB).
+
+		\param[in] circle
+			The Circle object representing the circle.
+
+		\param[in] rect
+			The AABB object representing the axis-aligned bounding box.
+
+		\return
+			True if the circle intersects with the AABB, false otherwise.
+
+		*/
+		/**************************************************************************/
 		bool CollisionIntersection_CircleRect(const Circle& circle, const AABB& rect);
 
 	};
