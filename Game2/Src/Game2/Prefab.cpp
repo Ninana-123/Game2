5f28a6bfb8169ff662434cbf30116cc48a032428
tristan1672 <115226386+tristan1672@users.noreply.github.com>
2023-09-29 01:08:26 +0800

    Prefab function

--- Game2/Src/Game2/Prefab.cpp
+++ Game2/Src/Game2/Prefab.cpp
@@ -3,6 +3,20 @@
 
 namespace Engine
 {
+	void Prefab::AddNewComponent(ComponentType type)
+	{
+		auto newComponent = ComponentFactory::CreateComponent(type);
+
+		if (newComponent)
+		{
+			// Emplace the new component into the entity's container
+			components.emplace(type, std::move(newComponent));
+		}
+		else
+		{
+			std::cout << "Failed to add new component." << std::endl;
+		}
+	}
 	std::unordered_map<ComponentType, Component*> Prefab::GetComponents() const
 	{
 		std::unordered_map<ComponentType, Component*> result;
--- Game2/Src/Game2/Prefab.h
+++ Game2/Src/Game2/Prefab.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "pch.h"
 #include "Component.h"
+#include "ComponentFactory.h"
 
 namespace Engine
 {
 	class Prefab
 	{
 	public:
+
+    void AddNewComponent(ComponentType type);
+
 	std::unordered_map<ComponentType, Component*> GetComponents() const;
 
 	std::unordered_map<ComponentType, std::unique_ptr<Component>> components;
