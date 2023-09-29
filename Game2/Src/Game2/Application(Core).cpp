2738d4878749fa70e0dab03f8d2b0eea55b0bbc4
Liu Xujie <liu_xujie@hotmail.sg>
2023-09-29 13:11:01 +0800

    Added comments/headers

--- Game2/Src/Game2/Application(Core).cpp
+++ Game2/Src/Game2/Application(Core).cpp
@@ -1,3 +1,19 @@
+/******************************************************************************/
+/*!
+\file		Application.cpp
+\author 	Liu Xujie
+\par    	email: l.xujie@digipen.edu
+\date   	29/09/2923
+\brief		This file contains the implementation of the main application class
+            and its functionalities, including window initialization, event 
+            handling, audio, graphics systems, and entity management.
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+ */
+ /******************************************************************************/
+//Includes
 #include "pch.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -15,28 +31,28 @@
 #include "AudioEngine.h"
 #include "Loader.h"
 
-double fps = 0.00;  // Frames per second
+// Global variables for frames per second (fps) calculation
+double fps = 0.00;
 double previousTime = glfwGetTime();  // Previous time for FPS calculation
 double dt = 0.0;  // Time difference between frames (delta time)
 
 namespace Engine
 {
     std::unique_ptr<Loader> loader;
-    //Window Properties
-    Engine::WindowConfig windowProps = loader->LoadWindowPropsFromConfig("config.txt");
+    // Window Properties configuration loaded from a file
+    Engine::WindowConfig windowProps = loader->LoadWindowPropsFromConfig("Resource/Config/config.txt");
 
-    //Set filepath of audio to the variable
+    // Audio file paths and SoundInfo objects
     AudioEngine audioEngine;
     SoundInfo sound("Resource/Audio/mainmenu_song.wav", "01");
     SoundInfo sound2("Resource/Audio/levelwin.wav", "02");
 
-    // Create a logger instance
     Engine::Logger logger;
     Engine::Input InputHandler;
     GraphicsSystem graphicsSystem;
     std::unique_ptr<ImGuiWrapper> m_ImGuiWrapper;
 
-    //Entity instances
+    // Entity-related instances and properties
     Engine::EntityManager EM;
     Engine::SystemsManager SM;
     EntityID cloneEntity;
@@ -44,24 +60,35 @@ namespace Engine
     TransformComponent* transformTest;
     ComponentFactory CF;
 
+    // Constants for transformation and scaling
     float scalar = 0.5f;
     float rotation = 0.125f;
     int transformation = 5;
-    bool currentlyPlayingSound = 0;
 
+    // Flag to track if a sound is currently playing
+    bool currentlyPlayingSound = 0;
+    /*!**********************************************************************
+    \brief
+    Constructor for the Application class
+    *************************************************************************/
     Application::Application()
     {
-        logger.Log(Engine::LogLevel::Debug, "Logger Initialized.");
     }   
-
+    /*!**********************************************************************
+    \brief
+    Destructor for the Application class
+    *************************************************************************/
     Application::~Application()
     {
-        // Destructor
     }
-
+    /*!**********************************************************************
+    \brief
+    Initialize the application
+    This function initializes various components and systems needed 
+    for the application to run.
+    *************************************************************************/
     void Application::Initialize()
     {
-       
         // Initialize GLFW
         if (!glfwInit()) {
             logger.Log(Engine::LogLevel::Error, "Failed to initialize GLFW");
@@ -75,32 +102,37 @@ namespace Engine
             return; // Handle the window creation error
         }
 
+        // Set event callback
         m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
 
-        //Systems Manager Initialization
-        //Currently initializes TestSystem and Graphics
+        // Systems Manager Initialization: initializes TestSystem and Graphics
         SM.Initialize();
 
+        // Load scene from a file
         loader = std::make_unique<Engine::Loader>(&EM);
         logger.Log(LogLevel::Debug, "Loading Scene");
-        loader->LoadScene("testscene.txt");
+        loader->LoadScene("Resource/Scenes/testscene.txt");
         logger.Log(LogLevel::Debug, "Scene Loaded");
         targetEntity = EM.GetEntity(0);
-        transformTest = dynamic_cast<TransformComponent*>(targetEntity->GetComponent(ComponentType::Transform)); //reference to Entity Transform data
+        transformTest = dynamic_cast<TransformComponent*>(targetEntity->GetComponent(ComponentType::Transform)); // Reference to Entity Transform data
 
-        //initialize audio files
+        // Initialize audio files and load sounds
         audioEngine.init();
-        //load both audio 
         audioEngine.loadSound(sound);
         audioEngine.loadSound(sound2);
         sound.setLoop();
         sound2.setLoop();
 
+        // Initialize ImGuiWrapper
         m_ImGuiWrapper = std::make_unique<Engine::ImGuiWrapper>(&EM);
         m_ImGuiWrapper->OnAttach();
         m_ImGuiWrapper->SetTargetEntity(targetEntity);
     }
-
+    /*!**********************************************************************
+    \brief
+    Event handler for processing events
+    This function handles incoming events and dispatches them accordingly.
+    *************************************************************************/
     void Application::OnEvent(Event& e)
     {
         // Event handler
@@ -110,6 +142,12 @@ namespace Engine
        m_ImGuiWrapper->OnEvent(e);
     }
 
+    /*!**********************************************************************
+    \brief
+    Run the application
+    This function runs the main loop of the application, handling input,
+    updating systems, and rendering.
+    *************************************************************************/
     void Application::Run()
     {
         logger.Log(Engine::LogLevel::App, "Application Running.");
@@ -118,11 +156,13 @@ namespace Engine
         while (m_Running)
         {
 
+            // Update input, window, delta time, and window title
             InputHandler.Update();
             m_Window->OnUpdate();
             Application::UpdateDeltaTime();
             Application::UpdateWindowTitle();
 
+            // Audio handling based on key input
             if (currentlyPlayingSound == false) {
                 if (InputHandler.IsKeyTriggered(KEY_9)) {
                     audioEngine.playSound(sound);
@@ -157,7 +197,8 @@ namespace Engine
                 SM.ToggleSystemState<CollisionSystem>();
             }
 
-            transformTest = dynamic_cast<TransformComponent*>(m_ImGuiWrapper->TargetEntityGetter()->GetComponent(ComponentType::Transform)); //reference to Entity Transform data
+            // Entity transformation based on input
+            transformTest = dynamic_cast<TransformComponent*>(m_ImGuiWrapper->TargetEntityGetter()->GetComponent(ComponentType::Transform));
 
 
             if (InputHandler.IsKeyPressed(KEY_UP))
@@ -204,8 +245,7 @@ namespace Engine
                 transformTest->scaleY -= scalar;
             }
 
-
-            //System Updating
+            // Update systems and ImGui
             SM.UpdateSystems(EM.GetEntities());
 
             //Entity Debug
@@ -217,7 +257,16 @@ namespace Engine
 
         }
     }
-
+    /*!**********************************************************************
+    \brief
+    Handles the window close event.
+    This function handles the event triggered when the application's
+    window is closed.
+    \param[in] e 
+    WindowCloseEvent object containing event information.
+    \return
+    True if the window close event was handled successfully, false otherwise.
+    *************************************************************************/
     bool Application::OnWindowClose(WindowCloseEvent& e)
     {
         UNREFERENCED_PARAMETER(e);
@@ -225,13 +274,23 @@ namespace Engine
         m_Running = false;
         return true;
     }
-
+    /*!**********************************************************************
+    \brief
+    Handles the window resize event.
+    This function handles the event triggered when the application's
+    window is resized.
+    \param[in] e
+    WindowResizeEvent object containing event information.
+    *************************************************************************/
     void Application::OnWindowResize(WindowResizeEvent& e)
     {
         // Update the viewport and projection matrix
         graphicsSystem.UpdateViewport(e.GetWidth(), e.GetHeight());
     }
-
+    /*!**********************************************************************
+    \brief
+    Updates the delta time and calculates frames per second (FPS).
+    *************************************************************************/
     void Application::UpdateDeltaTime()
     {
         static int frameCount = 0;
@@ -247,7 +306,10 @@ namespace Engine
             previousTime = currentTime;
         }
     }
-
+    /*!**********************************************************************
+    \brief
+    Updates the window title to display FPS.
+    *************************************************************************/
     void Application::UpdateWindowTitle() 
     {
         // Update the window title with FPS
