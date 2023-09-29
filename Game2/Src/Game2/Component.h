25b709f333e0be7f08beaf2f464a8e5b5b515379
Liu Xujie <liu_xujie@hotmail.sg>
2023-09-29 17:57:05 +0800

    Documentation

--- Game2/Src/Game2/Component.h
+++ Game2/Src/Game2/Component.h
@@ -1,3 +1,16 @@
+/******************************************************************************/
+/*!
+\file		Component.h
+\author 	Tristan Tham Rui Hong
+\par    	email: l.xujie@digipen.edu
+\date   	29/09/2923
+\brief		Declaration of the Component class.
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+ */
+ /******************************************************************************/
 #pragma once
 #include "EngineTypes.h"
 
--- Game2/Src/Game2/Entity.cpp
+++ Game2/Src/Game2/Entity.cpp
@@ -1,14 +1,38 @@
+/******************************************************************************/
+/*!
+\file		Entity.cpp
+\author 	Tristan Tham Rui Hong
+\par    	email: l.xujie@digipen.edu
+\date   	29/09/2923
+\brief		Definition of the Entity class.
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+ */
+ /******************************************************************************/
 #include "pch.h"
 #include "Entity.h"
 #include "Component.h"
 
 namespace Engine
 {
+	/*!**********************************************************************
+	\brief
+	Adds a component to the entity.
+	\param
+	component to add 
+	*************************************************************************/
 	void Entity::AddComponent(std::unique_ptr<Component> component)
 	{
 		components.emplace(component->GetType(), std::move(component));
 	}
-
+	/*!**********************************************************************
+	\brief
+	Adds a new component of the specified type to the entity.
+	\param
+	type of component to add
+	*************************************************************************/
 	void Entity::AddNewComponent(ComponentType type)
 	{
 		auto newComponent = ComponentFactory::CreateComponent(type);
@@ -23,7 +47,12 @@ namespace Engine
 			std::cout << "Failed to add new component." << std::endl;
 		}
 	}
-
+	/*!**********************************************************************
+	\brief
+	GetComponent of entity
+	\param
+	type of component to get
+	*************************************************************************/
 	Component* Entity::GetComponent(ComponentType type) const
 	{
 		auto it = components.find(type);
@@ -34,7 +63,10 @@ namespace Engine
 		}
 		else return nullptr;
 	}
-
+	/*!**********************************************************************
+	\brief
+	GetComponents from map
+	*************************************************************************/
 	std::unordered_map<ComponentType, Component*> Entity::GetComponents() const
 	{
 		std::unordered_map<ComponentType, Component*> result;
@@ -43,7 +75,12 @@ namespace Engine
 		}
 		return result;
 	}
-
+	/*!**********************************************************************
+	\brief
+	check for component
+	\return
+	true if have
+	*************************************************************************/
 	bool Entity::HasComponent(ComponentType type) const
 	{
 		return components.find(type) != components.end();
