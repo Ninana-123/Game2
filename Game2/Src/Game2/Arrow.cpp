0818a4a1fdbceefdd0810d5c0398d711843eb847
digistrax <vancemon@gmail.com>
2023-11-03 00:30:51 +0800

    arrow

--- Game2/Src/Game2/Arrow.cpp
+++ Game2/Src/Game2/Arrow.cpp
@@ -1,17 +1,20 @@
 #include "pch.h"
 #include "Arrow.h"
-#include "WindowsWindow.h"
+#include <GLFW/glfw3.h>
+#include "Window.h"
 
 namespace Engine {
-
-	WindowsWindow window;
-	float ARROW_HALF_X = window.GetWidth() / 2.0f;
-	float ARROW_HALF_Y = window.GetHeight() / 2.0f;
+	
+	WindowConfig window;
+	float ARROW_HALF_X = window.Width / 2.0f;
+	float ARROW_HALF_Y = window.Height / 2.0f;
 
 #define ARROW_BOUNDARY_X (ARROW_HALF_X + 100)
 #define ARROW_BOUNDARY_Y (ARROW_HALF_Y + 100)
 
 #define ARROW_SPEED 200.f
+
+
 
 	void Remove_Arrow(int index, ArrowPool& pool) {
 		pool.activeArrow[index]->enabled = false;
@@ -50,6 +53,12 @@ namespace Engine {
 	}
 
 	void AI_Arrow(ArrowPool& pool) {
+
+		static double lastTime = glfwGetTime();
+		double currentTime = glfwGetTime();
+		double deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+
 		for (int i = 0; i < pool.activeSize; i++) {
 			pool.activeArrow[i]->transform.position += pool.activeArrow[i]->direction * deltaTime * ARROW_SPEED;
 			if (pool.activeArrow[i]->transform.position.x > ARROW_BOUNDARY_X) {
