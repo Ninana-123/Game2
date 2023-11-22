d40e8352351e5e65825ef21a5cd2d7404e17ead6
Aloy <aloyang123@gmail.com>
2023-11-23 05:23:45 +0800

    Changed layer to tower for 2nd tower, WIP texturecheck for entityID

--- Game2/Src/Game2/EntityManager.cpp
+++ Game2/Src/Game2/EntityManager.cpp
@@ -109,5 +109,16 @@ namespace Engine
 			entities = std::move(updatedEntities);
 		}
 	}
+
+	//TextureClass EntityManager::GetTextureClassFromEntityID(EntityID entityID) const
+	//{
+	//	Entity* entity = GetEntity(entityID);
+	//	if (entity) {
+	//		// Assuming there is a function in the Entity class to get the TextureClass
+	//		return entity->GetTextureClass();
+	//	}
+	//	// Return an appropriate default value or handle the case where the entity is not found
+	//	return TextureClass::TextureClassCount;
+	//}
 }
 
