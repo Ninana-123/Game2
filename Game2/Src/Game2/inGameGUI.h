634ad439e5431d0058149a62f8b03be37a02d60e
Aloy <aloyang123@gmail.com>
2023-11-27 11:19:13 +0800

    added header comments

--- Game2/Src/Game2/inGameGUI.h
+++ Game2/Src/Game2/inGameGUI.h
@@ -1,10 +1,10 @@
 /******************************************************************************/
 /*!
 \file		inGameGUI.h
-\author
-\par    	email:
-\date
-\brief
+\author     Ang Jun Sheng Aloysius, a.junshengaloysius, 2201807
+\par		a.junshengaloysius@digipen.edu
+\date		06/11/2023
+\brief		Contains the declarations for the logic of the in game UI
 
             Copyright (C) 2023 DigiPen Institute of Technology.
             Reproduction or disclosure of this file or its contents without the prior
@@ -37,55 +37,61 @@ namespace Engine
     {
     public:
 
+        /**************************************************************************/
         /*!
-         * \brief Constructor for the inGameGUI class.
-         *
-         * Initializes an instance of the inGameGUI class.
-         */
+        \brief Default constructor for the in-game GUI.
+        */
+        /**************************************************************************/
         inGameGUI();
 
+        /**************************************************************************/
         /*!
-         * \brief Constructor for the inGameGUI class with additional parameters.
-         *
-         * Initializes an instance of the inGameGUI class with additional parameters.
-         *
-         * \param em A shared pointer to the EntityManager.
-         * \param pm A pointer to the PrefabManager.
-         */
+        \brief Parameterized constructor for the in-game GUI.
+
+        \param[in] em
+        A shared pointer to the EntityManager.
+
+        \param[in] pm
+        A pointer to the PrefabManager.
+        */
+        /**************************************************************************/
         inGameGUI(std::shared_ptr<Engine::EntityManager> em, Engine::PrefabManager* pm) : entityManager(em), prefabManager(pm) {}
 
+        /**************************************************************************/
         /*!
-         * \brief Sets the target entity for inGameGUI interactions.
-         *
-         * This function sets the target entity for inGameGUI interactions.
-         *
-         * \param entity A pointer to the target entity.
-         */
+        \brief Sets the target entity for the in-game GUI.
+
+        \param[in] entity
+        Pointer to the target entity.
+        */
+        /**************************************************************************/
         inline void SetTargetEntity(Entity* entity) { targetEntity = entity; }
 
+        /**************************************************************************/
         /*!
-         * \brief Gets the target entity for inGameGUI interactions.
-         *
-         * This function returns the target entity for inGameGUI interactions.
-         *
-         * \return A pointer to the target entity.
-         */
+        \brief Gets the target entity for the in-game GUI.
+
+        \return
+        Pointer to the target entity.
+        */
+        /**************************************************************************/
         inline Entity* TargetEntityGetter() { return targetEntity; }
 
+        /**************************************************************************/
         /*!
-         * \brief Initializes the inGameGUI.
-         *
-         * This function initializes the inGameGUI, including the CollisionSystem and other required components.
-         */
+        \brief Initializes the in-game GUI.
+        */
+        /**************************************************************************/
         void Initialize();
 
+        /**************************************************************************/
         /*!
-         * \brief Updates the inGameGUI.
-         *
-         * This function updates the inGameGUI, including collision checking if specified.
-         *
-         * \param CollisionCheck A boolean indicating whether to perform collision checking during the update.
-         */
+        \brief Updates the in-game GUI.
+
+        \param[in] CollisionCheck
+        Flag indicating whether collision checks should be performed.
+        */
+        /**************************************************************************/
         void Update(bool CollisionCheck);
 
         // ~inGameGUI();
