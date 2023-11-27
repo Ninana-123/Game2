b8232d1f3c2bd9f7ad9f2492190818aecc6544cf
digistrax <vancemon@gmail.com>
2023-11-27 09:16:28 +0800

    Revert "Merge branch 'Sheen-new'"
    This reverts commit e4371497de548b3ef18bcc3ad2c81e5c07269a4d, reversing
    changes made to e0fa6216a8b9bca74d84a302fdb45c27138e5592.

--- Game2/Src/Game2/GraphicsSystem.cpp
+++ Game2/Src/Game2/GraphicsSystem.cpp
@@ -388,7 +388,7 @@ namespace Engine
             // You might want to add an else case to handle the situation where textureVector is empty
 
              //If Sprite present
-            if ((texture->textureKey.subIndex == 1 ) && entity->HasComponent(ComponentType::Sprite))
+            if ((texture->textureKey.subIndex == 1) && entity->HasComponent(ComponentType::Sprite))
             {
                 // Calculate deltaTime (time since the last frame)
                 static double lastTime = glfwGetTime();
@@ -493,76 +493,6 @@ namespace Engine
                 shader.SetUniform1f("u_FrameHeight", frameHeight);
                 shader.SetUniform1i("u_CurrentFrame", currentFrame);
             }
-            if ((texture->textureKey.subIndex == 4) && entity->HasComponent(ComponentType::Sprite))
-            {
-                // Calculate deltaTime (time since the last frame)
-                static double lastTime = glfwGetTime();
-                double currentTime = glfwGetTime();
-                double deltaTime = currentTime - lastTime;
-                lastTime = currentTime;
-                float frameRate = 10.0f;
-                float horizontalFrames = 5.0f; // Number of horizontal frames
-                float verticalFrames = 1.0f; // Number of vertical frames
-                //float Length = 1536.0f; // length of sprite sheet
-                Anim_Mode playMode = Anim_Mode::LOOP;
-
-                // Create a static animation object if not created already
-                static Animation animation(frameRate, horizontalFrames, verticalFrames, playMode);
-                // Play the animation
-                animation.Play();
-                // Update the animation with deltaTime
-                animation.Update(static_cast<float>(deltaTime));
-                // Get the current frame index
-                int currentFrame = animation.GetCurrentFrame();
-
-                // Calculate the texture offset based on the current frame
-                float frameWidth = 1.0f / horizontalFrames;
-                float frameHeight = 1.0f / verticalFrames;
-                float texCoordX = currentFrame * frameWidth;
-                //float texCoordY = currentRow * frameHeight;
-
-                // Set the texture offset in the shader
-                shader.SetUniform1f("texCoordX", texCoordX);
-                //shader.SetUniform1f("u_FrameCount", horizontalFrames);
-                shader.SetUniform1f("u_FrameWidth", frameWidth);
-                shader.SetUniform1f("u_FrameHeight", frameHeight);
-                shader.SetUniform1i("u_CurrentFrame", currentFrame);
-            }
-            if ((texture->textureKey.subIndex == 5) && entity->HasComponent(ComponentType::Sprite))
-            {
-                // Calculate deltaTime (time since the last frame)
-                static double lastTime = glfwGetTime();
-                double currentTime = glfwGetTime();
-                double deltaTime = currentTime - lastTime;
-                lastTime = currentTime;
-                float frameRate = 10.0f;
-                float horizontalFrames = 6.0f; // Number of horizontal frames
-                float verticalFrames = 1.0f; // Number of vertical frames
-                //float Length = 1536.0f; // length of sprite sheet
-                Anim_Mode playMode = Anim_Mode::LOOP;
-
-                // Create a static animation object if not created already
-                static Animation animation(frameRate, horizontalFrames, verticalFrames, playMode);
-                // Play the animation
-                animation.Play();
-                // Update the animation with deltaTime
-                animation.Update(static_cast<float>(deltaTime));
-                // Get the current frame index
-                int currentFrame = animation.GetCurrentFrame();
-
-                // Calculate the texture offset based on the current frame
-                float frameWidth = 1.0f / horizontalFrames;
-                float frameHeight = 1.0f / verticalFrames;
-                float texCoordX = currentFrame * frameWidth;
-                //float texCoordY = currentRow * frameHeight;
-
-                // Set the texture offset in the shader
-                shader.SetUniform1f("texCoordX", texCoordX);
-                //shader.SetUniform1f("u_FrameCount", horizontalFrames);
-                shader.SetUniform1f("u_FrameWidth", frameWidth);
-                shader.SetUniform1f("u_FrameHeight", frameHeight);
-                shader.SetUniform1i("u_CurrentFrame", currentFrame);
-            }
             else //render as static
             {
                 textures[texture->textureKey.mainIndex][0].Bind(0); //render static version of texture at subindex = 0
@@ -613,7 +543,7 @@ namespace Engine
             vaLines.Bind();
             shader.SetUniform1i("u_RenderTextured", 0); // no texture
             if (renderCollisionBox == true) {
-                shader.SetUniform4f("u_Color", 0.0f, 0.0f, 0.0f, 0.0f); // Set the line color
+                shader.SetUniform4f("u_Color", 0.0f, 0.0f, 0.0f, 1.0f); // Set the line color
             }
             else
                 shader.SetUniform4f("u_Color", 0.0f, 0.0f, 0.0f, 0.0f); // Set the line color
