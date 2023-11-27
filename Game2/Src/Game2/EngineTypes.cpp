3eeff8801f77750701491817f108df528ee86962
Ninana-123 <113290028+Ninana-123@users.noreply.github.com>
2023-11-27 08:58:17 +0800

    tower animation

--- Game2/Src/Game2/EngineTypes.cpp
+++ Game2/Src/Game2/EngineTypes.cpp
@@ -15,11 +15,13 @@ std::string c_stateToString(c_state state)
 {
     switch (state)
     {
-    case c_state::Static:   return "Static";
-    case c_state::Walking:  return "Walking";
-    case c_state::Idle:     return "Idle";
-    case c_state::Attack:   return "Attack";
-    case c_state::Death:   return "Death";
-    default:                return "Unknown";
+    case c_state::Static:           return "Static";
+    case c_state::Walking:          return "Walking";
+    case c_state::Idle:             return "Idle";
+    case c_state::Attack:           return "Attack";
+    case c_state::Death:            return "Death";
+    case c_state::CastleDestroyed:  return "CastleDestroyed";
+    case c_state::TowerDestroyed:   return "TowerDestroyed";
+    default:                        return "Unknown";
     }
 }
