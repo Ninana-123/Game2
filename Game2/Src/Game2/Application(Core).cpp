32a726e64c6c390ec22dfb60264050db6c084da4
waynekjl <kjlwayne@gmail.com>
2024-04-07 18:11:54 +0800

    removed debug box, sort of fixed the interruption handling (just that towers will somehow disappear after window loses focus, cutscene cmi

--- Game2/Src/Game2/Application(Core).cpp
+++ Game2/Src/Game2/Application(Core).cpp
@@ -331,7 +331,7 @@ namespace Engine
                     if (IsPointInQuadrilateral(mouseX, mouseY, 603, 305, 719, 308, 725, 367, 597, 353))
                     {
               
-                        fp = CutSceneFilePath;
+                        fp = GameSceneFilePath;
                         int entityCount = static_cast<int>(EM->GetEntities()->size());
 
                         for (int i = entityCount - 1; i >= 0; --i) {
@@ -349,8 +349,8 @@ namespace Engine
 
                         // Now load the scene
                         loader->LoadScene(fp);
-                        sceneManager.TransitionToScene(std::make_shared<CutSceneLevel>(EM, &PM, assetManager));
-                        sceneManager.UpdateScene(std::make_shared<CutSceneLevel>(EM, &PM, assetManager));
+                        //sceneManager.TransitionToScene(std::make_shared<CutSceneLevel>(EM, &PM, assetManager));
+                        //sceneManager.UpdateScene(std::make_shared<CutSceneLevel>(EM, &PM, assetManager));
 
                         if (EM->GetEntities()->size() >= 2) {
                             m_ImGuiWrapper->selectedEntityIndex = 1;
